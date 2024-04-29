d27477781e2167967b239483541b9b32deef1a0a
DeLi0v <denov78@mail.ru>
2024-04-29 13:31:38 +0300

    Nav bar

---
+++ d.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="ru">
+
+<head>
+    <?php include_once ("MainHead.php") ?>
+</head>
+
+<body>
+    <div class="nav">
+        <a hrev="#">Clients</a>
+        <a hrev="#">Staff</a>
+        <a hrev="#">Rooms</a>
+        <a hrev="#">Booking</a>
+    </div>
+    <div class="content">
+
+    </div>
+</body>
+
+</html>

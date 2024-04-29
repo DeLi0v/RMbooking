e02c94ac23efc1ae5065d1b42c5076b770ecac8b
DeLi0v <denov78@mail.ru>
2024-04-29 13:13:15 +0300

    sidenav

--- index.php
+++ index.php
@@ -1,48 +1,19 @@
+<!doctype html>
+<html lang="ru">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Centered Div with Buttons</title>
-    <style>
-        body,
-        html {
-            height: 100%;
-            margin: 0;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-        }
-
-        .center-div {
-            width: 300px;
-            background-color: lightgray;
-            padding: 20px;
-            text-align: center;
-        }
-
-        .button {
-            display: inline-block;
-            padding: 10px 20px;
-            margin: 10px;
-            background-color: #4CAF50;
-            color: white;
-            border: none;
-            border-radius: 4px;
-            cursor: pointer;
-            text-decoration: none;
-            font-size: 16px;
-        }
-
-        .button:hover {
-            background-color: #45a049;
-        }
-    </style>
+    <?php include_once ("MainHead.php") ?>
 </head>
 
 <body>
-    <div class="center-div">
-        <button class="button">Кнопка 1</button>
-        <button class="button">Кнопка 2</button>
-        <button class="button">Кнопка 3</button>
+    <div class="nav">
+        <a hrev="#">Clients</a>
+        <a hrev="#">Staff</a>
+        <a hrev="#">Rooms</a>
+        <a hrev="#">Booking</a>
+    </div>
+    <div class="content">
+
     </div>
 </body>
 

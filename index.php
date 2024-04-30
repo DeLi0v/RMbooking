2e802ca0e8bfa04fccfd9174a19b597fa737a0ba
DeLi0v <denov78@mail.ru>
2024-04-30 21:43:49 +0300

    jquery

--- index.php
+++ index.php
@@ -11,17 +11,11 @@
     <?php include_once ("./navBar.php") ?>
 
     <div class="content">
-        <script>
-            if (window.jQuery) {
-                console.log("ok");
-            } else {
-                console.log("err");
-            }
-        </script>
         <?php include_once ("./pages/booking.php") ?>
     </div>
 
-    <script src="./js/jquery-3.7.1.js"></script>
+    <!-- <script src="./js/jquery-3.7.1.js"></script> -->
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <script src="./js/openPage.js"></script>
 </body>
 

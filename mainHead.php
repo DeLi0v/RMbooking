9579f488dd52f51a774307a4e51b0c076293d23c
DeLi0v <denov78@mail.ru>
2024-04-30 21:04:49 +0300

    install jquery

--- mainHead.php
+++ mainHead.php
@@ -4,3 +4,6 @@
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <title>RMbooking</title>
 <link rel="stylesheet" href="/styles/main.css">
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"
+      integrity="sha384-KyZXEAg3QhqLMpG8r+Knujsl5+M5OqED/Se+Y9LvNXiQ+BrqBv/6pBZd6P5K+JEL"
+      crossorigin="anonymous"></script>

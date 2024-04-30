067fe68962adab72ddf541af12285312a844d41b
DeLi0v <denov78@mail.ru>
2024-04-30 23:31:12 +0300

    select from clients

--- pages/clients.php
+++ pages/clients.php
@@ -1,12 +1,4 @@
-<table>
-    <tr>
-        <th>id</th>
-        <th>Фамилия</th>
-        <th>Имя</th>
-        <th>Отчество</th>
-        <th>Телефон</th>
-        <th>Почта</th>
-    </tr>
+
 
     <?php
     require_once("../connect.php"); // Подключение файла для связи с БД
@@ -16,14 +8,44 @@
     $conn = $db->connect();
     mysqli_select_db($conn, $db->database);
 
-    echo "<tr>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-            <td>4</td>
-            <td>5</td>
-            <td>6</td>
-        </tr>"
-        ?>
+    $sql = "SELECT 
+                id AS id,
+                surname AS surname,
+                name AS name,
+                patronymic AS patronymic,
+                phone AS phone,
+                email AS email
+            FROM Clients";
 
-</table>
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) { ?>
+    <table>
+    <tr>
+        <th>id</th>
+        <th>Фамилия</th>
+        <th>Имя</th>
+        <th>Отчество</th>
+        <th>Телефон</th>
+        <th>Почта</th>
+    </tr>
+    
+    <?php
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "<tr>";
+        echo "<td>" . $row["id"] . "</td>";
+        echo "<td>" . $row["surname"] . "</td>";
+        echo "<td>" . $row["name"] . "</td>";
+        echo "<td>" . $row["patronymic"] . "</td>";
+        echo "<td>" . $row["phone"] . "</td>";
+        echo "<td>" . $row["email"] . "</td>";
+        echo "</tr>";
+    }
+    ?>
+
+    </table>
+    <?php
+    } else {
+        echo "<div>В таблице нет данных.</div>";
+    } 
+    ?>

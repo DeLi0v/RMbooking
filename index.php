c46c339594f529ba3f053343274e3b2a882dfc0d
DeLi0v <denov78@mail.ru>
2024-04-30 22:34:52 +0300

    jq

--- index.php
+++ index.php
@@ -15,11 +15,12 @@
 
     <!-- TODO: разобраться с подключением локальной копией JQuery  -->
 
+    <!-- <script src="/js/jquery.min.js"></script> -->
+    <script src="/js/jquery-3.7.1.slim.min.js"></script>
     <!-- <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script> -->
-    <script src="https://code.jquery.com/jquery-3.7.1.slim.min.js"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.7.1.slim.min.js"></script> -->
     <script src="/js/openPage.js"></script>
 
-    <!-- <script src="/js/jquery.min.js"></script> -->
 </body>
 
 </html>

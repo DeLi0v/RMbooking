eb366849dab2378e5677c45727a02dbf9c926d3b
DeLi0v <denov78@mail.ru>
2024-04-30 21:26:58 +0300

    fix path

--- mainHead.php
+++ mainHead.php
@@ -3,5 +3,5 @@
       content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <title>RMbooking</title>
-<link rel="stylesheet" href="./styles/main.css">
-<script defer src="./js/jquery-3.7.1.js"></script>
+<link rel="stylesheet" href="/styles/main.css">
+<script defer src="/js/jquery-3.7.1.js"></script>

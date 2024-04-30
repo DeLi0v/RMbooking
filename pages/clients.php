a3be89f2b85422863702677cb8d66c898fe9be92
DeLi0v <denov78@mail.ru>
2024-04-30 22:45:13 +0300

    add php code

--- pages/clients.php
+++ pages/clients.php
@@ -1,2 +1,19 @@
-<div>hello</div>
-<?php include ("./booking.php") ?>
+<table>
+    <tr>
+        <th>id</th>
+        <th>Фамилия</th>
+        <th>Имя</th>
+        <th>Отчество</th>
+        <th>Телефон</th>
+        <th>Почта</th>
+    </tr>
+    <?php
+    echo "<tr>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+        </tr>"
+        ?>
+</table>

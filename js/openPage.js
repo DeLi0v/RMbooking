7cbb01da0a53a65461ca7f75722efde729ccfcd5
DeLi0v <denov78@mail.ru>
2024-04-30 21:58:43 +0300

    fix openPage.js

--- js/openPage.js
+++ js/openPage.js
@@ -10,26 +10,5 @@ function openPage(evt, name) {
 
   const elem = document.querySelector(".content");
   elem.innerHTML = "";
-
-  // fetch("../pages/" + name + ".php")
-  //   .then(function (response) {
-  //     if (!response.ok) {
-  //       // Если статус не в диапазоне 200-299, генерируем ошибку
-  //       throw new Error("Network response was not ok");
-  //     }
-  //     // Преобразуем ответ в текст
-  //     return response.text();
-  //   })
-  //   .then(function (data) {
-  //     elem.innerHTML = data;
-  //   })
-  //   .catch(function (err) {
-  //     contentElement.innerHTML = "<p>Error loading content.</p>";
-  //     console.error(
-  //       "There has been a problem with your fetch operation:",
-  //       error
-  //     );
-  //   });
-
-  $(".content").load("../pages/" + name + ".php");
+  elem.load("../pages/" + name + ".php");
 }

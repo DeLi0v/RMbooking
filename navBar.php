5571fa8a761218c44ca430a618aa48e02250324c
DeLi0v <denov78@mail.ru>
2024-04-30 19:57:35 +0300

    change navBar

--- navBar.php
+++ navBar.php
@@ -1,6 +1,13 @@
-<ul class="navBar">
+<!-- <ul class="navBar">
     <li><a href="/index.php" class="tablinks" onclick="openPage(event, 'booking')">Бронь</a></li>
     <li><a href="/" class="tablinks" onclick="openPage(event, 'rooms')">Помещения</a></li>
     <li><a href="#" class="tablinks" onclick="openPage(event, 'clients')">Клиенты</a></li>
     <li><a href="#" class="tablinks" onclick="openPage(event, 'staff')">Сотрудники</a></li>
-</ul>
+</ul> -->
+
+<div class="navBar">
+    <button class="tablinks" onclick="openPage(event, 'booking')">Бронь</button>
+    <button class="tablinks" onclick="openPage(event, 'rooms')">Помещения</button>
+    <button class="tablinks" onclick="openPage(event, 'clients')">Клиенты</button>
+    <button class="tablinks" onclick="openPage(event, 'staff')">Сотрудники</button>
+</div>

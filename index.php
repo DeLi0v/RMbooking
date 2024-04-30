b809dc81f9f5ce929fa3eb9c75f8abf1d2b7d5f0
DeLi0v <denov78@mail.ru>
2024-04-30 21:42:37 +0300

    install jquery

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
 </head>
 
 <body>
-    <!-- <script defer src="/js/jquery-3.7.1.js"></script> -->
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+
+    <!-- <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script> -->
     <?php include_once ("./navBar.php") ?>
 
     <div class="content">
@@ -21,8 +21,8 @@
         <?php include_once ("./pages/booking.php") ?>
     </div>
 
+    <script src="./js/jquery-3.7.1.js"></script>
     <script src="./js/openPage.js"></script>
-
 </body>
 
 </html>

c49b4b1a8ea67ffacf7205c18301420f6448f54d
DeLi0v <denov78@mail.ru>
2024-05-01 17:26:38 +0300

    add script

--- index.php
+++ index.php
@@ -21,7 +21,28 @@
     <script src="/js/jquery-mask.js"></script>
     <!-- <script src="https://code.jquery.com/jquery-3.7.1.slim.min.js"></script> -->
     <script src="/js/openPage.js"></script>
+    <script>
+        // Получаем все обязательные поля формы
+        var requiredFields = document.querySelectorAll("#editForm [required]");
 
+        // Добавляем обработчик события invalid для каждого обязательного поля
+        requiredFields.forEach(function(field) {
+        field.addEventListener("invalid", function() {
+            // Добавляем класс invalid-input к незаполненному полю
+            field.classList.add("invalid-input");
+        });
+        });
+
+        // Добавляем обработчик события input для каждого обязательного поля, чтобы убрать подсветку при вводе
+        requiredFields.forEach(function(field) {
+        field.addEventListener("input", function() {
+            // Убираем класс invalid-input у поля при вводе данных
+            if (field.validity.valid) {
+            field.classList.remove("invalid-input");
+            }
+        });
+        });
+    </script>
 </body>
 
 </html>

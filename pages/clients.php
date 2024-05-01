d43544f2e7b4ff4bc78b2158749246b9b669c970
DeLi0v <denov78@mail.ru>
2024-05-01 12:54:50 +0300

    add class center

--- pages/clients.php
+++ pages/clients.php
@@ -35,7 +35,7 @@
             echo "<td>" . $row["patronymic"] . "</td>";
             echo "<td>" . $row["phone"] . "</td>";
             echo "<td>" . $row["email"] . "</td>";
-            echo '<td><input class="delete_edit" type="image" src="/pictures/remove.png" value="' . $row["id"] . '"></input></td>';
+            echo '<td class="center"><input type="image" src="/pictures/remove.png" value="' . $row["id"] . '"></input></td>';
             echo "</tr>";
         }
         ?>

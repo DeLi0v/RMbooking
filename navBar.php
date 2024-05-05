6f934af85d3eb49c52cfa4966b4f642df7ed1b15
DeLi0v <denov78@mail.ru>
2024-05-05 15:33:38 +0300

    fix first state

--- navBar.php
+++ navBar.php
@@ -1,5 +1,5 @@
 <div class="navBar">
-    <button class="tablinks active" id="booking" onclick="openPage('booking')">Бронь</button>
+    <button class="tablinks" id="booking" onclick="openPage('booking')">Бронь</button>
     <button class="tablinks" id="rooms" onclick="openPage('rooms')">Помещения</button>
     <button class="tablinks" id="clients" onclick="openPage('clients')">Клиенты</button>
     <button class="tablinks" id="staff" onclick="openPage('staff')">Сотрудники</button>
